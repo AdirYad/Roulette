7b4fef3f7d132534b18678db321d07ea16fe6293
AdirYed <adir.yed@gmail.com>
2020-08-21 21:45:35 +0300

    added countdown

--- src/Roulette.js
+++ src/Roulette.js
@@ -5,13 +5,54 @@ import './assets/roulette_gold.png';
 import './Roulette.css';
 
 class Roulette extends Component {
+  state = {
+    seconds: 20,
+    milliseconds: 0,
+  };
+
+  componentDidMount() {
+    this.myInterval = setInterval(() => {
+      console.log('hi')
+      const { seconds, milliseconds } = this.state
+      if (seconds > 0) {
+        if (milliseconds === 0) {
+          this.setState(({ seconds }) => ({
+            seconds: seconds - 1,
+            milliseconds: 99
+          }));
+        } else {
+          this.setState(({ milliseconds }) => ({
+            milliseconds: milliseconds - 1
+          }));
+        }
+      } else if (milliseconds > 0) {
+        this.setState(({ milliseconds }) => ({
+          milliseconds: milliseconds - 1
+        }));
+      }
+    }, 10);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.myInterval)
+  }
+
   render() {
+    const countdown = `${this.state.seconds}.${this.state.milliseconds}`;
+
+    const spinItems = [];
+
+    for (let i = 0; i < 3; i++) {
+      spinItems.push(<div className="spin red" />);
+      spinItems.push(<div className="spin blue" />);
+    }
+
     return (
       <div className="App">
         <div className="roulette">
           <div className="roulette-roller">
             <div className="rolling">Rolling</div>
-            <div className="countdown">15</div>
+            <div className="countdown">{ countdown }</div>
           </div>
 
           <div className="roulette-mask" />
@@ -19,19 +60,9 @@ class Roulette extends Component {
           <div className="roulette-indicator hidden" />
 
           <div className="roulette-spin-items">
-            <div className="spin red" />
-            <div className="spin blue" />
-            <div className="spin red" />
-            <div className="spin blue" />
-            <div className="spin red" />
-            <div className="spin blue" />
+            { spinItems }
             <div className="spin gold" />
-            <div className="spin red" />
-            <div className="spin blue" />
-            <div className="spin red" />
-            <div className="spin blue" />
-            <div className="spin red" />
-            <div className="spin blue" />
+            { spinItems }
           </div>
         </div>
       </div>
